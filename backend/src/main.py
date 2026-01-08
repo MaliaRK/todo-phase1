34e5fdf3779bde6916aa39050c068dc2a667b86d
Malia Raees <160169166+MaliaRK@users.noreply.github.com>
2026-01-09 02:34:58 +0500

    Change default port from 8080 to 8000

--- backend/src/main.py
+++ backend/src/main.py
@@ -24,5 +24,5 @@ def read_root():
     return {"message": "Todo API is running!"}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Read PORT env variable
+    port = int(os.environ.get("PORT", 8000))  # Read PORT env variable
     uvicorn.run("src.main:app", host="0.0.0.0", port=port, reload=True)
